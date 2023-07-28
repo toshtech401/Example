fecf3b5e2bdc470485a939cb3b12a8606806ae41
unknown <toshconsultinc@gmail.com>
2023-07-28 03:53:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './Components/About';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Home from './Components/Home';
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Home />
+      <About />
       <Footer />
     </div>
   );
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,31 +1,10 @@
-import React, {useState} from 'react'
-import "./Header.css"
+import React from 'react'
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
-
-
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <div className="logo">TechCorp</div>
-        <div>
-        <button className="navbar-toggle" onClick={toggleNavbar}>
-          <span className="icon"></span>
-        </button>
-        </div>
-      </div>
-      <ul className={`navbar-links ${isOpen ? 'active' : ''}`}>
-        <li><a href="">Home</a></li>
-        <li><a href="">About</a></li>
-        <li><a href="">Services</a></li>
-        <li><a href="">Contact</a></li>
-      </ul>
-    </nav>
+    <div>
+      
+    </div>
   )
 }
 
